7b92ae32b27da339cf0482f0629e02c40747b5e1
nitin <nitinarrow7@gmail.com>
2025-09-18 17:27:13 +0000

    added c project

---
+++ rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse() {
+    char str[100], temp;
+    int i, len;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    len = strlen(str);
+
+    // Reverse the string
+    for (i = 0; i < len / 2; i++) {
+        temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+
+    printf("Reversed string: %s\n", str);
+
+//    return 0;
+}
+
