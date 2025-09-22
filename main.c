e9332232c94cd56a013e2b19bbd17c89af4574a9
nitinarrow7 <nitinarrow7@gmail.com>
2025-09-22 07:46:56 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
-main() {
-	biggest3();
-	factorial();
-	reverse();
-}
+#include <stdio.h>
 
+// Function declarations
+void biggest3();
+void factorial();
+void reverse();
+
+int main() {
+    biggest3();
+    factorial();
+    reverse();
+    return 0;
+}
